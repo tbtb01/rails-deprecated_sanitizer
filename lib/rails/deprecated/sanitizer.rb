dcb9a8e75c6a4296c91de497f6bae29c5040ab29
Timm <kaspth@gmail.com>
2014-05-23 23:33:32 +0200

    Initial commit.

---
+++ lib/rails/deprecated/sanitizer.rb
@@ -0,0 +1,9 @@
+require "rails/deprecated/sanitizer/version"
+
+module Rails
+  module Deprecated
+    module Sanitizer
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/deprecated/sanitizer/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Deprecated
+    module Sanitizer
+      VERSION = "0.0.1"
+    end
+  end
+end
