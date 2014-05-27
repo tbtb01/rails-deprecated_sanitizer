44ca0edf1157b87807df0fae0341ecb895996022
Timm <kaspth@gmail.com>
2014-05-27 13:52:26 +0200

    Actually require the sanitizer.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,7 @@ require 'active_support'
 require 'active_support/test_case'
 require 'active_support/testing/autorun'
 
+require 'rails/deprecated_sanitizer'
+
 # Show backtraces for deprecated behavior for quicker cleanup.
 ActiveSupport::Deprecation.debug = true
