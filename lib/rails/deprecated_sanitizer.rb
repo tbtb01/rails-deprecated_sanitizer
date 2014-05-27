e942d31c160db0ff520a239cc7339b19d3135390
Timm <kaspth@gmail.com>
2014-05-27 10:37:05 +0200

    SanitizeHelper is in a Helpers module within ActionView.

--- lib/rails/deprecated_sanitizer.rb
+++ lib/rails/deprecated_sanitizer.rb
@@ -19,25 +19,27 @@ module Rails
 end
 
 module ActionView
-  module SanitizeHelper
-    def sanitized_allowed_tags
-      HTML::WhiteListSanitizer.allowed_tags
-    end
+  module Helpers
+    module SanitizeHelper
+      def sanitized_allowed_tags
+        HTML::WhiteListSanitizer.allowed_tags
+      end
 
-    def sanitized_allowed_tags=(*tags)
-      HTML::WhiteListSanitizer.allowed_tags = tags
-    end
+      def sanitized_allowed_tags=(*tags)
+        HTML::WhiteListSanitizer.allowed_tags = tags
+      end
 
-    def sanitized_allowed_attributes
-      HTML::WhiteListSanitizer.allowed_attributes
-    end
+      def sanitized_allowed_attributes
+        HTML::WhiteListSanitizer.allowed_attributes
+      end
 
-    def sanitized_allowed_attributes=(*attributes)
-      HTML::WhiteListSanitizer.allowed_attributes = attributes
+      def sanitized_allowed_attributes=(*attributes)
+        HTML::WhiteListSanitizer.allowed_attributes = attributes
+      end
     end
   end
 end
 
 ActiveSupport.on_load(:action_view) do
-  ActionView::SanitizeHelper.sanitizer_vendor = Rails::DeprecatedSanitizer
+  ActionView::Helpers::SanitizeHelper.sanitizer_vendor = Rails::DeprecatedSanitizer
 end
